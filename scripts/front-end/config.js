a3b458ff9348ce6eccd29e5f39be0a96d947b090
Nathan Marianovsky <nathanmarianovsky@manualmath.com>
2022-11-24 00:56:59 -0800

    Cleaning Up

--- scripts/front-end/config.js
+++ scripts/front-end/config.js
@@ -6,23 +6,12 @@ require.config({
         jquery: "/node_modules/jquery/dist/jquery.min",
         materialize: "/bower_components/materializecss-amd/dist/materialize.amd.min",
         router5: "/node_modules/router5/dist/amd/router5.min",
-        mathjax: "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?...",
         math: "/node_modules/mathjs/lib/browser/math"
     },
     shim: {
         materialize: {
             exports: "Materialize",
             deps: ["jquery"]
-        },
-        mathjax: {
-            exports: "MathJax",
-            init: function () {
-                MathJax.Hub.Config({
-                    tex2jax: {inlineMath: [["$","$"], ["\\(","\\)"]]}
-                });
-                MathJax.Hub.Startup.onload();
-                return MathJax;
-            }
         }
     }
 });
--- scripts/front-end/main.js
+++ scripts/front-end/main.js
@@ -1,4 +1,4 @@
-define(["jquery", "materialize", "router5", "mathjax", "app/routes", "math", "app/functions"], ($, Materialize, router5, MathJax, routes, Math, functions) => {
+define(["jquery", "materialize", "router5", "app/routes", "math", "app/functions"], ($, Materialize, router5, routes, Math, functions) => {
 	$(function() {
 		var router = new router5.Router5([
 			new router5.RouteNode("def", "/")
